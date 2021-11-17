d4100cb0570bea071b295ac3e75f3aca7cd6f5bd
Dan Connolly <connolly@agoric.com>
2021-11-16 21:00:20 -0600

    docs: types for FP utilities

--- packages/treasury/test/test-runLoC.js
+++ packages/treasury/test/test-runLoC.js
@@ -21,9 +21,28 @@ import { makeRatio } from '@agoric/zoe/src/contractSupport/index.js';
 const { assign, entries, fromEntries, keys, values } = Object;
 const { details: X } = assert;
 
+/**
+ * @param {Record<string, V>} obj
+ * @param {(v: V) => U} f
+ * @returns {Record<string, U>}
+ * @template V
+ * @template U
+ */
 const mapValues = (obj, f) =>
   fromEntries(entries(obj).map(([p, v]) => [p, f(v)]));
+/**
+ * @param {X[]} xs
+ * @param {Y[]} ys
+ * @returns {[X, Y][]}
+ * @template X
+ * @template Y
+ */
 const zip = (xs, ys) => xs.map((x, i) => [x, ys[i]]);
+/**
+ * @param {Record<string, ERef<V>>} obj
+ * @returns {Promise<Record<string, V>>}
+ * @template V
+ */
 const allValues = async obj =>
   fromEntries(zip(keys(obj), await Promise.all(values(obj))));
 
